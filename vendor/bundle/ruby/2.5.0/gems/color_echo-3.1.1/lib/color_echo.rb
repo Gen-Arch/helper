803dfd05a372aab70216a581b0300e719729075c
tos.ito <tos.ito@ntt.com>
2018-10-22 17:04:28 +0900

    gem ver

---
+++ vendor/bundle/ruby/2.5.0/gems/color_echo-3.1.1/lib/color_echo.rb
@@ -0,0 +1,6 @@
+# color_echo
+# 
+# Decorates command line output with ANSI escape sequence.
+
+require_relative "color_echo/get"
+require_relative "color_echo/override"
---
+++ vendor/bundle/ruby/2.5.0/gems/color_echo-3.1.1/lib/color_echo/module/textattr.rb
@@ -0,0 +1,9 @@
+module CE
+    module TextAttr
+        BOLD          = "\033[1m"
+        UNDERLINE     = "\033[4m"
+        BLINK         = "\033[5m"
+        REVERSE_VIDEO = "\033[7m"
+        CONCEALED     = "\033[8m"
+    end
+end
